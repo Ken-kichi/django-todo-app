30ee18f07c1b6923cb9819fa9a38884afd66b426
Ken-kichi <PoTi15673cmgpjwp@yahoo.co.jp>
2023-08-06 15:11:01 +0900

    コメントを追加

--- todo_project/todo/models.py
+++ todo_project/todo/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+# Todoに入れるデータを定義する。
 class TodoTask(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True,null=True)
--- todo_project/todo/views.py
+++ todo_project/todo/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import TodoTask
 from .forms import TodoTaskForm
 
+# Todoのリストを表示する機能
 def todo_list(request):
     tasks = TodoTask.objects.all()
     return render(request, 'todo/todo_list.html', {'tasks': tasks})
 
+# Todoを作成する機能
 def todo_create(request):
     if request.method == 'POST':
         form = TodoTaskForm(request.POST)
@@ -16,6 +18,7 @@ def todo_create(request):
         form = TodoTaskForm()
     return render(request, 'todo/todo_form.html', {'form': form})
 
+# Todoを更新する機構
 def todo_update(request, pk):
     task = get_object_or_404(TodoTask, pk=pk)
     if request.method == 'POST':
@@ -27,6 +30,7 @@ def todo_update(request, pk):
         form = TodoTaskForm(instance=task)
     return render(request, 'todo/todo_form.html', {'form': form})
 
+# Todoを削除する機能
 def todo_delete(request, pk):
     task = get_object_or_404(TodoTask, pk=pk)
     if request.method == 'POST':
